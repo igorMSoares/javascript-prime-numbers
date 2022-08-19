267de953bf79438218f9e850c95ec48f8bed5db7
iguhSoares <iguhsoares@gmail.com>
2022-08-19 17:40:19 -0300

    passing primesList as cached prime numbers and returning itat the end

--- primeNumbers.js
+++ primeNumbers.js
@@ -1,30 +1,91 @@
 function multipleOf(num1, num2) {
-  return (Math.max(num1, num2) % Math.min(num1, num2)) === 0 ? true : false;
+  return Math.max(num1, num2) % Math.min(num1, num2) === 0 ? true : false;
 }
 
+function isPrimeNumber(number, primesList = false) {
+  console.log(`Inicio: ${number}`);
+  if (primesList) {
+    console.log([...primesList]);
+  }
+  if (Number.isNaN(number)) {
+    if (typeof number === 'string') {
+      number = parseInt(number);
+    } else {
+      throw 'Invalid type error';
+    }
+  }
 
-function verifyPrimeNumber(number, upTo=null, outputType="string") {
-  if (!["array", "string"].includes(outputType)) {
+  if (number <= 1 || (number !== 2 && number % 2 === 0)) {
+    return false;
+  }
+
+  primesList = primesList || new Set();
+  //  primesList.add(2);
+
+  let numbersToTest = [];
+  for (let n = 3; n <= number; n += 2) {
+    numbersToTest.push(n);
+  }
+  if (numbersToTest.length === 0) {
+    primesList.add(number);
+  }
+
+  let limit = number ** 0.5;
+
+  console.log({ limit });
+  console.log({ numbersToTest });
+  for (let i = 0; numbersToTest[i] <= limit; i++) {
+    let num = numbersToTest[i];
+    console.log({ num });
+    if (primesList.has(num) || isPrimeNumber(num, primesList)) {
+      if (primesList.has(num)) {
+        console.log('cache dentro do for');
+      }
+      primesList.add(num);
+      numbersToTest = numbersToTest.filter(n => !multipleOf(n, num));
+      console.log(`num: ${num}`);
+      console.log(numbersToTest);
+      i = -1;
+    }
+  }
+  primesList = new Set([...primesList, ...numbersToTest]);
+
+  if (primesList.has(number)) {
+    return [true, primesList];
+  }
+  return [false, primesList];
+}
+
+function verifyPrimeNumber(number, upTo = null, outputType = 'string') {
+  if (!['array', 'string'].includes(outputType)) {
     throw new InvalidArgumentException(
       'Invalid outputType: choose "array" or "string"'
     );
   }
-  if (upTo === 0) { upTo = null; }
-  if (upTo !== null) { // generates a list of the upTo first prime numbers
+  if (upTo === 0) {
+    upTo = null;
+  }
+  if (upTo !== null) {
+    // generates a list of the upTo first prime numbers
     if (number !== 0) {
       try {
         return generatePrimeNumbersList(number, upTo, outputType);
-      } catch(error) { return console.log(error.message); }
+      } catch (error) {
+        return console.log(error.message);
+      }
     }
     if (!validate(upTo)[0]) {
-      throw new InvalidArgumentException(validate(upTo)["message"]);
+      throw new InvalidArgumentException(validate(upTo)['message']);
     }
 
     var generatingListOfPrimes = true;
     var sqrt = Math.floor(Math.sqrt(upTo));
     var totalNumbers = upTo;
-  } else { // verifies if number is prime
-    if (number === 0 || number === 1 ||  number < 0) { return false; }
+  } else {
+    // verifies if number is prime
+    if (number === 0 || number === 1 || number < 0) {
+      return false;
+    }
     var generatingListOfPrimes = false;
 
     var sqrt = Math.floor(Math.sqrt(number));
@@ -32,22 +93,24 @@ function verifyPrimeNumber(number, upTo=null, outputType="string") {
   }
 
   var primes = [];
-  for (let i=2; i <= totalNumbers; i++) {
+  for (let i = 2; i <= totalNumbers; i++) {
     primes.push(i);
   }
-  if (primes.length === 0) { return true; }
+  if (primes.length === 0) {
+    return true;
+  }
 
   var tmp = [];
   let num = null;
-  for (let i = 0 ; primes[i] <= sqrt ; i++) {
+  for (let i = 0; primes[i] <= sqrt; i++) {
     num = primes[i];
 
     if (verifyPrimeNumber(num)) {
       if (!generatingListOfPrimes && multipleOf(number, num)) {
         return false;
       }
-      primes = primes.filter((n) => !multipleOf(num,n));
-      i=-1; // back to begining of new primes list
+      primes = primes.filter(n => !multipleOf(num, n));
+      i = -1; // back to begining of new primes list
       tmp.push(num); // tmp stores already tested prime numbers
     }
   }
@@ -57,17 +120,16 @@ function verifyPrimeNumber(number, upTo=null, outputType="string") {
   }
 
   primes = tmp.concat(primes);
-  if (outputType == "string") {
-    return primes.join(", ");
+  if (outputType == 'string') {
+    return primes.join(', ');
   } else {
     return primes;
   }
 }
 
-
 function verificaNumeroPrimo(num) {
   if (!validate(num)[0]) {
-    throw new InvalidArgumentException(validate(num)["message"]);
+    throw new InvalidArgumentException(validate(num)['message']);
   }
 
   var simPrimo = `O número ${num} é primo.`;
@@ -76,48 +138,52 @@ function verificaNumeroPrimo(num) {
   return verifyPrimeNumber(num) ? simPrimo : naoPrimo;
 }
 
-
-function generatePrimeNumbersList(from, to, outputType="string") {
-  if (!["array", "string"].includes(outputType)) {
-    throw new InvalidArgumentException('Invalid outputType: choose "array" or "string"');
+function generatePrimeNumbersList(from, to, outputType = 'string') {
+  if (!['array', 'string'].includes(outputType)) {
+    throw new InvalidArgumentException(
+      'Invalid outputType: choose "array" or "string"'
+    );
   }
   if (!validate(from)[0]) {
-    throw new InvalidArgumentException(validate(from)["message"]);
+    throw new InvalidArgumentException(validate(from)['message']);
   }
   if (!validate(to)[0]) {
-    throw new InvalidArgumentException(validate(to)["message"]);
+    throw new InvalidArgumentException(validate(to)['message']);
   }
   if (from > to) {
     let tmp = from;
     from = to;
     to = tmp;
   }
-  if ([0,1,2].includes(from)) { return verifyPrimeNumber(0, to, outputType); }
+  if ([0, 1, 2].includes(from)) {
+    return verifyPrimeNumber(0, to, outputType);
+  }
 
-  var list1 = verifyPrimeNumber(0, from, "array");
-  var list2 = verifyPrimeNumber(0, to, "array");
+  var list1 = verifyPrimeNumber(0, from, 'array');
+  var list2 = verifyPrimeNumber(0, to, 'array');
 
-  var res = list2.filter((n) => { return (n === from || !list1.includes(n))});
-  if (outputType === "string") {
-    return res.join(", ");
+  var res = list2.filter(n => {
+    return n === from || !list1.includes(n);
+  });
+  if (outputType === 'string') {
+    return res.join(', ');
   }
   return res;
 }
 
-
 function validate(num) {
-  var response = { 0: true, "message": "" };
+  var response = { 0: true, message: '' };
 
   if (!Number.isInteger(num) || num < 0) {
     response[0] = false;
-    response["message"] = "Entrada inválida.\n"+
-              "Apenas números inteiros positivos serão computados.";
+    response['message'] =
+      'Entrada inválida.\n' +
+      'Apenas números inteiros positivos serão computados.';
   }
   return response;
 }
 
-
 function InvalidArgumentException(message) {
   this.message = message;
-  this.name = "InvalidArgumentException";
+  this.name = 'InvalidArgumentException';
 }
